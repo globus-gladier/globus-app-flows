9f9fa3eda90ca7e236dd030be538c02c8e5b48ef
Nickolaus D. Saint <nick@globus.org>
2023-11-02 10:12:53 -0700

    feat: Slightly better background working behavior
    Workers will now attempt to gracefully shutdown on SIGINT. They will
    likely be killed before they have a chance to finish, but this should
    be OK, since run workers don't keep critical data. Collectors may be
    killed mid-collection, which would be rare but would casue duplicate
    runs, which could be a problem we need to fix later

--- globus_app_flows/worker.py
+++ globus_app_flows/worker.py
@@ -1,15 +1,22 @@
 import threading
 import logging
 import time
+import signal
 
 log = logging.getLogger(__name__)
 
 
 class SingleThreadedWorker:
+    shutdown_flag = False
     workers = []
     max_workers = 2
 
+    def shutdown(self, *args):
+        log.debug(f"Shutdown initiated for {self}")
+        self.shutdown_flag = True
+
     def start(self):
+        signal.signal(signal.SIGINT, self.shutdown)
         if len(self.workers) >= self.max_workers:
             log.debug(f"{self}: Additional worker attempted to start, aborting!")
             return
@@ -19,9 +26,8 @@ class SingleThreadedWorker:
 
     def _run_thread(self):
         time.sleep(5)
-        while True:
+        while self.shutdown_flag is False:
             self.do_work()
-            log.debug(f"{self} tick...")
             time.sleep(5)
         log.info(f"{self}: Shutdown successful")
 
